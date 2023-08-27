34b0fab2ee5dd6248eeb346a483bf31465166d09
Braulio <braulio-03@live.com.mx>
2023-08-27 14:44:14 -0600

    segundo commit

--- src/test/java/mainCurp/main.java
+++ src/test/java/mainCurp/main.java
@@ -5,18 +5,37 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        String primerNombre = "JOSE";
-        String segundoNombre = "BRAULIO";
-        String apellidoPaterno = "VILLARREAL";
-        String apellidoMaterno = "BARRON";
-        String fechaNacimiento = "26/03/1995";
 
-        String a =  apellidoPaterno.substring(0,2);
+        String primerNombre;
+        String segundoNombre;
+        String apellidoPaterno;
+        String apellidoMaterno;
+        String fechNac;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Esbribe tu primer nombre: ");
+        primerNombre = sc.nextLine();
+
+        System.out.println("Esbribe tu segundo nombre: ");
+        segundoNombre = sc.nextLine();
+
+        System.out.println("Esbribe tu apellido paterno: ");
+        apellidoPaterno = sc.nextLine();
+
+        System.out.println("Esbribe tu apellido materno: ");
+        apellidoMaterno = sc.nextLine();
+
+        System.out.println("Esbribe tu feccha de nacimiento en formato dd/mm/aaaa: ");
+        fechNac = sc.nextLine();
+
+
+        String a = apellidoPaterno.substring(0,2);
         String b = apellidoMaterno.substring(0,1);
         String c = primerNombre.substring(0,1);
-        String d = fechaNacimiento.substring(8,10);
-        String e = fechaNacimiento.substring(3,5);
-        String f = fechaNacimiento.substring(0,2);
+        String d = fechNac.substring(8,10);
+        String e = fechNac.substring(3,5);
+        String f = fechNac.substring(0,2);
 
          System.out.println("Mi Curp es: " + a+b+c+d+e+f);
     }
